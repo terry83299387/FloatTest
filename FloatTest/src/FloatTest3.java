b98d10d616051c30059af46c5ffcae3f722f60c5
Antineutrino <83299387@163.com>
2015-05-24 01:10:11 +0800

    create project

---
+++ FloatTest/src/FloatTest3.java
@@ -0,0 +1,23 @@
+/**
+ * 该程序修正了原程序中的问题。
+ * 
+ * @author mkqiao
+ *
+ */
+public class FloatTest3 {
+
+	/**
+	 * 由于去除了强制转换，因此不会导致原程序中由于截断所引起的错误。
+	 * 同时该程序简单、直接，容易理解，因此我推荐用这种方法。
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		double opacity = 0;
+		while (opacity < 1) {
+			opacity += 0.01;
+			System.out.println("opacity=" + opacity);
+		}
+	}
+
+}
