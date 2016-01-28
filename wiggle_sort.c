a42866792f48c92c6d0404750ad0a1b3f2913149
Qiao <83299387@163.com>
2016-01-28 16:28:33 +0800

    Update wiggle_sort.c
    optimized & refactor

--- wiggle_sort.c
+++ wiggle_sort.c
@@ -72,7 +72,7 @@ void swapLastNums(int *nums, int size, int i, int needGreater) {
 	}
 }
 
-// 不需要了解该函数的细节，见greaterThan()中的注释即可
+// 不需要了解该函数的细节，见sort()中的注释即可
 int findNotEqualOne(int *nums, int size, int i) {
 	int num = nums[i];
 	for (i++; i < size; i++) {
@@ -85,70 +85,59 @@ int findNotEqualOne(int *nums, int size, int i) {
 }
 
 /*
- * 判断是否满足大于关系，不满足时调整顺序
- */
-void greaterThan(int *nums, int size, int i) {
-	int n;
-
-	if (nums[i] < nums[i-1]) { // 发现两者是小于关系，交换一下即可调整为大于关系
-		swap(nums, i, i - 1);
-	} else if (nums[i] == nums[i-1]) { // 两者相等时候的处理较为麻烦一些
-		// 首先尝试从后面找出一个与当前元素不相等的元素
-		n = findNotEqualOne(nums, size, i);
-		// 返回值不等于-1说明找到了这样的元素
-		if (n != -1) {
-			// 首先交换当前元素nums[i]和找到的那个元素nums[n]
-			swap(nums, i, n);
-			// 然后再调用一次greaterThan()函数判断交换后的值与前一个元素哪个更大
-			// 这里不这样递归调用也行，只是要多写一个if/else语句而已
-			greaterThan(nums, size, i);
-		} else {
-			// ************【【【【这里很关键】】】】************
-			// ************【【【【这里很关键】】】】************
-			// ************【【【【这里很关键】】】】************
-			//
-			// n等于-1说明没有找到与当前元素不相等的元素，这就说明后面剩下的元素全部是相等的！！
-			// 这时候需要把这些元素以每次间隔一个的顺序与前面合适的元素交换。见swapLastNums()函数
-			//
-			// ************【【【【这里很关键】】】】************
-			// ************【【【【这里很关键】】】】************
-			// ************【【【【这里很关键】】】】************
-			swapLastNums(nums, size, i, 1);
-		}
-	} // 这里还有个else，不需要处理，因为已经是大于关系了
-}
-
-/*
- * 判断是否满足小于关系，不满足时调整顺序（算法与greaterThan()函数一样，就是关系是反过来的而已）
- */
-void lessThan(int *nums, int size, int i) {
-	int n;
-
-	if (nums[i] > nums[i-1]) {
-		swap(nums, i, i - 1);
-	} else if (nums[i] == nums[i-1]) {
-		n = findNotEqualOne(nums, size, i);
-		if (n != -1) {
-			swap(nums, i, n);
-			lessThan(nums, size, i);
-		} else {
-			swapLastNums(nums, size, i, -1);
-		}
-	}
-}
-
-/*
  * 遍历数组，将每一个元素与它前面的元素进行比较，并在必要的时候调整元素的顺序
  * 数组遍历一遍之后就已经排好序了
  */
 void sort(int *nums, int size) {
+	int n;
 	int i = 1;
+	// 交替判断是否满足大于和小于关系，不满足时需要调整顺序
 	while (i < size) {
-		// 交替判断是否满足大于和小于关系，不满足时需要调整顺序
-		if (i % 2 == 1) {
-			greaterThan(nums, size, i);
-		} else {
-			lessThan(nums, size, i);
+		if (i % 2 == 1) { // 索引为奇数的元素需要比它前面的元素大
+			if (nums[i] < nums[i-1]) { // 比前一个元素小，交换一下即可调整为大于关系
+				swap(nums, i, i - 1);
+			} else if (nums[i] == nums[i-1]) { // 两者相等时候的处理较为麻烦一些
+				// 首先尝试从后面找出一个与当前元素不相等的元素
+				n = findNotEqualOne(nums, size, i);
+				if (n != -1) { // 返回值不等于-1说明找到了这样的元素
+					// 根据元素n是较大还是较小来决定将其与元素i还是元素i-1交换
+					if (nums[i] < nums[n]) {
+						swap(nums, i, n);
+					} else {
+						swap(nums, i - 1, n);
+					}
+				} else {
+					// ************【【【【这里很关键】】】】************
+					// ************【【【【这里很关键】】】】************
+					// ************【【【【这里很关键】】】】************
+					//
+					// n等于-1说明没有找到与当前元素不相等的元素，这就说明后面剩下的元素全部是相等的！！
+					// 这时候需要把这些元素以每次间隔一个的顺序与前面合适的元素交换。见swapLastNums()函数
+					//
+					// ************【【【【这里很关键】】】】************
+					// ************【【【【这里很关键】】】】************
+					// ************【【【【这里很关键】】】】************
+					swapLastNums(nums, size, i, 1);
+					break;
+				}
+			} // 这里还有个else，不需要处理，因为已经比前一个元素要大了
+
+		} else { // 判断是否比前一个元素小，不满足时调整顺序（算法与前面一样，就是关系是反过来的而已）
+			if (nums[i] > nums[i-1]) {
+				swap(nums, i, i - 1);
+			} else if (nums[i] == nums[i-1]) {
+				n = findNotEqualOne(nums, size, i);
+				if (n != -1) {
+					if (nums[i] > nums[n]) {
+						swap(nums, i, n);
+					} else {
+						swap(nums, i - 1, n);
+					}
+				} else {
+					swapLastNums(nums, size, i, -1);
+					break;
+				}
+			}
 		}
 
 		i++;
@@ -178,7 +167,6 @@ void printResult() {
 }
 
 int main(int argc,char **argv) {
-
 	sort(NUMS, SIZE);
 
 	printResult();
